d7148ca30c87f46159bd35d1517e1ccc451bc50b
nazartal <nazartal@yandex.ru>
2019-09-03 23:48:17 +0700

    repair confest with language options

--- conftest.py
+++ conftest.py
@@ -1,18 +1,27 @@
 import pytest
 from selenium import webdriver
 
-def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': 'language'})
+browser = webdriver.Chrome(options=options)
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: ru, en,...(etc.)")
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     browser = None
     if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': 'language'})
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
