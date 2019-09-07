f6ffd6d3ccf7c9754f7af130b239123dc8bda27c
nazartal <nazartal@yandex.ru>
2019-09-07 11:15:24 +0700

    update test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -7,10 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link) 
     page.open()
     page.go_to_login_page()
-    page2 = LoginPage(browser, link)
-    page2.should_be_login_url()
-    page2.should_be_login_form()
-    page2.should_be_register_form()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
     
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
